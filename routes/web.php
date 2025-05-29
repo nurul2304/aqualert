508293069f8fea3ed0475629119eac61af12419b
nurul2304 <159009520+nurul2304@users.noreply.github.com>
2025-05-29 20:41:00 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::get('/visualdata', function () {
+    return view('visualdata');
+})->name('visualdata');
+
+Route::get('/laporan', function () {
+    return view('laporan');
+})->name('laporan');
+
+Route::get('/notifikasi', function () {
+    return view('notifikasi');
+})->name('notifikasi');
